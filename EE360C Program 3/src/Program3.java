13da46da302caeb86154f033d1fea71306e575b3
Kristopher Williams <kwilliams@utexas.edu>
2013-05-01 02:07:03 -0500

    Additional Help File Added

--- EE360C Program 3/src/Program3.java
+++ EE360C Program 3/src/Program3.java
@@ -3,29 +3,49 @@ public class Program3 implements IProgram3{
 	int noOfClasses;
 	int maximumGrade;
 	GradeFunction gradeCalc; 
-		
+			
 	@Override
 	public void initialize(int numClasses, int maxGrade, GradeFunction gf) {
 		//GradesvsHours = new int[numClasses][numClasses];
 		noOfClasses = numClasses;
 		maximumGrade = maxGrade;	
 		gradeCalc = gf;
+		
 	}
 
 	@Override
 	public int[] computeHours(int totalHours) {
 		//Must establish a matrix to track grades versus hours spent in classes
-		int[][] Hours_vs_Classes = new int[noOfClasses][totalHours];
+		int[][] potentialGradeArray = initalizeGradeArray(totalHours);
+		int[][] premiumAllocations = new int[totalHours][noOfClasses];
 		
-		for(int i = 0; i < noOfClasses; i++){
-			for(int j = 0; j < totalHours; j++){
-				Hours_vs_Classes[i][j] = gradeCalc.grade(i, j);
+		int[] optimumGrades = new int[totalHours];
+		
+		for(int i = 1; i < totalHours; i++){
+			int topGradeSoFar = 0;
+			int premiumJ = 0;
+			int premiumK = 0;
+			
+			for(int j = 0; j < noOfClasses; j++){
+				
+				for(int k = 1; k < i; k++){
+					int potentialGrade = potentialGradeArray[k][j];
+					int gradeSum = optimumGrades[i] - potentialGrade + potentialGradeArray[(i-k)][j];
+					if(gradeSum > topGradeSoFar){
+						topGradeSoFar = gradeSum;
+						premiumJ = j;
+						premiumK = k;
+					}
+				}
 			}
+			premiumAllocations[i] = optimumGrades;	
+			premiumAllocations[i][premiumK] = premiumAllocations[i][premiumK] + i - premiumJ;
+			optimumGrades[i] = topGradeSoFar;
 		}
 		
-		for(int i = 0; i < noOfClasses; i++){
-			for(int j = 0; j < totalHours; j++){
-				System.out.print(Hours_vs_Classes[i][j] + " ");
+		for(int i = 0; i < totalHours; i++){
+			for(int j = 0; j < noOfClasses; j++){
+				System.out.print(premiumAllocations[i][j] + " ");
 			}
 			System.out.println("\n");
 		}
@@ -38,5 +58,15 @@ public class Program3 implements IProgram3{
 		return null;
 	}
 	
-
+	private int[][] initalizeGradeArray(int totalHours) {
+		int[][] potentialGradeArray = new int[totalHours][noOfClasses];
+		
+		for(int i = 0; i < totalHours; i++){
+			for(int j = 0; j < noOfClasses; j++){
+				potentialGradeArray[i][j] = gradeCalc.grade(j, i);
+			}
+		}
+		
+		return potentialGradeArray;		
+	}
 }
